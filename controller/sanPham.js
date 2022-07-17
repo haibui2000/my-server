db971ec553696cae8d37e2fa8ed5557448587e4a
haibui2000 <buivanhai250520@gmail.com>
2022-07-17 16:13:39 +0700

    update _createSanPham

--- controller/sanPham.js
+++ controller/sanPham.js
@@ -19,7 +19,7 @@ export const _getDataToSeasons = async (req, res) => {
         var perPage = Math.max(0, req.query.perPage);
         var pagenumber = Math.max(0, req.query.pagenumber);
         var seasonType = req.query.seasonType
-        const value = await ModelSanPham.find({seasons: 1}).limit(perPage).skip(perPage * pagenumber).sort({ "createdAt": -1 });
+        const value = await ModelSanPham.find({ seasons: 1 }).limit(perPage).skip(perPage * pagenumber).sort({ "createdAt": -1 });
         res.status(200).send({
             dataSP: value,
         });
@@ -44,10 +44,10 @@ export const _search = async (req, res) => {
 }
 export const _getSPBestSeller = async (req, res) => {
     try {
-        var perPage = Math.max(0, req.query.perPage); 
+        var perPage = Math.max(0, req.query.perPage);
         var pagenumber = Math.max(0, req.query.pagenumber);
 
-        const value = await ModelSanPham.find().limit(perPage).skip(perPage * pagenumber).sort({sold: -1 });
+        const value = await ModelSanPham.find().limit(perPage).skip(perPage * pagenumber).sort({ sold: -1 });
         res.status(200).send({
             dataSP: value,
         });
@@ -58,13 +58,15 @@ export const _getSPBestSeller = async (req, res) => {
 
 export const _createSanPham = async (req, res) => {
     try {
-        const {backImg, name, weightMax, unit, nguyenLieu, origin, nsx, typeSP, price, sale, seasons, title} = req.body
-        const priceSale  = req.body.priceSale??0
-        const sold  = req.body.sold??0
+        const { backImg, name, weightMax, unit, nguyenLieu, origin, nsx, typeSP, price, sale, seasons, title } = req.body
+        const priceSale = req.body.priceSale ?? 0
+        const sold = req.body.sold ?? 0
+        const URL = `https://server-demo-native.herokuapp.com/backgrounds/`;
+
         const post = new ModelSanPham({
             id: new mongoose.Types.ObjectId(),
             img: "https://cdn.pixabay.com/photo/2016/09/13/09/25/kite-1666816_960_720.jpg",
-            backImg: backImg,
+            backImg: URL + backImg,
             arrImg: [
                 'https://cdn.pixabay.com/photo/2022/06/04/19/57/river-7242735_960_720.jpg',
                 'https://cdn.pixabay.com/photo/2022/05/23/18/45/bee-7216939_960_720.jpg',
@@ -82,9 +84,9 @@ export const _createSanPham = async (req, res) => {
             },
             typeSP: typeSP,
             price: price,
-            priceSale: priceSale??0,
+            priceSale: priceSale ?? 0,
             sale: sale,
-            sold: sold??0,
+            sold: sold ?? 0,
             seasons: seasons
         })
         post.save();
